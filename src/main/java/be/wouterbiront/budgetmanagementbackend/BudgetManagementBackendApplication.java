2cf5175bd0cbfc395b846d520cfdc2833d918649
Wouter Biront <wouterbiront@gmail.com>
2025-12-14 12:18:29 +0100

    Initial commit

---
+++ src/main/java/be/wouterbiront/budgetmanagementbackend/BudgetManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package be.wouterbiront.budgetmanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetManagementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetManagementBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/wouterbiront/budgetmanagementbackend/BudgetManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package be.wouterbiront.budgetmanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
