e2c825d60b349a9dd4b533f386bb55e895152529
Wouter Biront <wouterbiront@gmail.com>
2026-01-02 20:30:22 +0100

    User's budget can be assigned after creation

--- domain/src/main/java/be/wouterbiront/budgetmanagement/features/user/model/User.java
+++ domain/src/main/java/be/wouterbiront/budgetmanagement/features/user/model/User.java
@@ -9,18 +9,24 @@ public class User {
     private final String id;
     private final String firstName;
     private final String lastName;
-    private final Budget budget;
+    private Budget budget;
 
     @Builder
     private User(String id, String firstName, String lastName,  Budget budget) {
         if (id == null || id.isBlank()) throw new IllegalArgumentException("id is required");
         if (firstName == null || firstName.isBlank()) throw new IllegalArgumentException("firstName is required");
         if (lastName == null || lastName.isBlank()) throw new IllegalArgumentException("lastName is required");
-        if (budget == null) throw new IllegalArgumentException("budget is required");
 
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.budget = budget;
+    }
+
+    public void assignBudget(Budget budget) {
+        if (budget == null) throw new IllegalArgumentException("budget is required");
+        if (this.budget != null) throw new IllegalArgumentException("budget is already assigned");
+
         this.budget = budget;
     }
 }
